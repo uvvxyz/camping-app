f6fc973a3d54ca2ad460f83076beb80305f0ae68
uvvxyz <23ee02007@iitbbs.ac.in>
2024-03-02 08:45:52 +0530

    Refactor code for improved performance and readability

---
+++ src/main.js
@@ -0,0 +1,7 @@
+function main() {
+  let destination = "Crater Lake";
+  destination = "Sparks Lake";
+  console.log(destination);
+}
+
+main();
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let mut destination = "Crater Lake";
+    println!("{} is the deepest lake in the US", destination);
+    destination = "Sparks Lake";
+    println!("{} is also a lake in the US", destination);
+}
